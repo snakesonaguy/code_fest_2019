8f2be8051c30838eafe29292cbe96482d03a7a7f
snakesonaguy <blasiols@oregonstate.edu>
2019-02-23 18:12:22 -0500

    modified:   http_script.py

--- http_script.py
+++ http_script.py
@@ -14,7 +14,41 @@ import time
 
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+random.seed()
 
+def get_index():
+    chosen = False
+
+    while (chosen != True):
+        num = random.randint(0, limit)
+        roll = random.randint (1, 10)
+        if num > (limit * 0.90):
+            if roll > 9:
+                chosen = True
+        elif num > (limit * 0.80):
+            if roll > 8:
+                chosen = True
+        elif num > (limit * 0.70):
+            if roll > 7:
+                chosen = True
+        elif num > (limit * 0.60):
+            if roll > 6:
+                chosen = True
+        elif num > (limit * 0.50):
+            if roll > 5:
+                chosen = True
+        elif num > (limit * 0.40):
+            if roll > 4:
+                chosen = True
+        elif num > (limit * 0.30):
+            if roll > 3:
+                chosen = True
+        elif num > (limit * 0.20):
+            if roll > 2:
+                chosen = True
+        else:
+            chosen = True
+    return num
 
 print '############ WELCOME TO THE BLAZE SIMPLE HTTP TRAFFIC GENERATOR ############'
 print '############# YOU CAN LIST WEBSITES IN THE \'websites.txt\' FILE #############'
@@ -35,13 +69,12 @@ in_file.close()
 
 limit = len(sites) - 1
 
-
-
 while True:
-    index = random.randint(0, limit)
+    index = get_index()
     dest = sites[index]
-    timer = random.randint(2, 4)
+    timer = random.randint(2,4)
     print 'Attempting GET to: ' + dest
+    print 'This site is ranked at: ', (index + 1)
     try:
         req = http.request('GET', dest)
         req.close()
